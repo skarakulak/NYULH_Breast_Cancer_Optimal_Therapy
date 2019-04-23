f2ecbb6b7c092c2bf0380815a01c9d76f4cf58aa
skarakulak <serkankarakulak@outlook.com>
2019-04-23 01:39:53 -0400

    prep report v0.5

--- utils.py
+++ utils.py
@@ -34,3 +34,35 @@ def div_cluster(coor,cls_labels, clus_id_div = 1, num_of_clus = 2, n_neighbors =
     dict_new_clus_ids = {b:(-1 if b == -1 else a) for a,b in enumerate(set(labels_temp))}
     labels_temp = np.array([dict_new_clus_ids[k] for k in labels_temp])
     return labels_temp
+
+
+def prep_report(medData_float,df_corr,train_ind, dummy_cols,target_cols, quantile_1 =.125, quantile_2 = .875 ):
+    report_df = pd.DataFrame(index = df_MI_X.columns)
+    t_float_vars = medData_float.iloc[train_ind]
+    for ii,clus in enumerate(target_cols):
+        report_df[clus+'_MI'] = mutual_info_regression(df_MI_X.astype('float64').values,df_corr[clus].astype('float64'), discrete_features=discrete_flag)
+        report_df[clus+'_COR'] = df_MI_X.astype('float64').corrwith(df_corr[clus])
+
+        # categ vars 
+        c_stats = np.zeros((len(dummy_cols),2))
+        for id1, lb1 in enumerate(dummy_cols): 
+            c_stats[id1,0] = ((df_corr[lb1] == 1)&(df_corr[clus] == 1)).sum() / (df_corr[lb1] == 1).sum()
+            c_stats[id1,1] = ((df_corr[lb1] == 1)&(df_corr[clus] == 1)).sum() / (df_corr[clus] == 1).sum()
+        c_stats = pd.DataFrame(c_stats,index =dummy_cols,  columns=[clus + s for s in ['_(%)Label','_(%)Cluster']])
+        report_df = pd.concat([report_df,c_stats],axis=1,sort=False)
+
+        # float vars
+        cls_float_vals = t_float_vars[(df_corr[clus]==1).values]
+        quartile_cols = [f'{clus}_Q{100*quantile_1:.3}%', f'{clus}_Q{100*quantile_2:.3}%']
+        report_df[quartile_cols]  = cls_float_vals.quantile([quantile_1,quantile_2]).T
+        report_df[clus+'_MEAN'] = cls_float_vals.mean()
+        report_df[clus+'_NON_NAN'] = cls_float_vals.count() / (df_corr[clus]==1).values.sum()
+        np_q_perc = np.zeros(report_df.shape[0])
+        for f_idx, f_col in enumerate(t_float_vars):
+            q_bool = (
+                (t_float_vars[f_col]>=report_df.loc[f_col,quartile_cols[0]]) &(t_float_vars[f_col]<=report_df.loc[f_col,quartile_cols[1]])
+            ).values
+            np_q_perc[f_idx] = (q_bool & (df_corr[clus]==1).values).sum() / q_bool.sum()
+        report_df[clus+'_(%)Percentile'] = np_q_perc
+    report_df['categ_long'] = pd.Series([dummy_cols_map[k] if k in dummy_cols_map else '-' for k in report_df.index ],index=report_df.index)
+    return report_df
